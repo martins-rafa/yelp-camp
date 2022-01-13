bc3e0ed05e6f9f3e5b2a0f875f3c9f2b6389cc4e
Rafa Martins <rafamartins.dev@gmail.com>
2022-01-13 17:29:57 -0300

    adding review mongoose Schema and updating campground Schema to link with the reviews db and delete them in case the campground is deleted

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Review = require("./review");
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
@@ -7,6 +8,23 @@ const CampgroundSchema = new Schema({
   price: Number,
   description: String,
   location: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+});
+
+// Delete the campground's reviews when the campground is deleted
+CampgroundSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
 });
 
 module.exports = mongoose.model("Campground", CampgroundSchema);
