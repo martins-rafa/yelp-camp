c7b5f26ae2c4e69a085c59409b8a619258b52b7f
Rafa Martins <rafamartins.dev@gmail.com>
2022-01-25 16:42:18 -0300

    updating seeds (random data generator)

--- seeds/index.js
+++ seeds/index.js
@@ -21,7 +21,7 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 // Generate random campgrounds
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 200; i++) {
     // generate a random number 1 - 1000
     const random1000 = Math.floor(Math.random() * 1000);
     const randomPrice = Math.floor(Math.random() * 20) + 10;
@@ -29,10 +29,27 @@ const seedDB = async () => {
     // using the random number, get the information needed to create a campground
     const camp = new Campground({
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: "https://source.unsplash.com/collection/483251",
       price: randomPrice,
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      geometry: {
+        type: "Point",
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude,
+        ],
+      },
+      author: "61e8759419bbf6f377605cc3",
+      images: [
+        {
+          url: "https://res.cloudinary.com/rafadatabase/image/upload/v1643034800/YelpCamp/camptesting_uegwyg.jpg",
+          filename: "YelpCamp/camptesting_uegwyg",
+        },
+        {
+          url: "https://res.cloudinary.com/rafadatabase/image/upload/v1643034808/YelpCamp/camptesting2_l69pw2.jpg",
+          filename: "YelpCamp/camptesting2_l69pw2",
+        },
+      ],
     });
 
     // Save camp to Database
